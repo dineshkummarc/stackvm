b7d34f3d96bfc9306cb80600cdab4a1b7f6577e1
James Halliday <substack@gmail.com>
2010-06-09 23:37:47 -0800

    manager can start and shutdown vms

--- bin/manager.js
+++ bin/manager.js
@@ -1,15 +1,47 @@
 #!/usr/bin/env node
 var sys = require('sys');
 var fs = require('fs');
-
-with ({ x : require('dnode') }) {
-    var DNode = x.DNode;
-    var Async = x.Async;
-}
+var proc = require('child_process');
+var DNode = require('dnode').DNode;
 
 function Manager () {
-    this.vmList = Async(function (f) {
-        fs.readdir(__dirname + '/../vm', function (err,files) { f(files) });
+    var manager = this;
+    var status = {};
+    var instances = {};
+    var vmDir = __dirname + '/../vm';
+    
+    process.addListener('exit', function () {
+        for (var vm in instances) {
+            manager.stop(vm);
+        }
+    });
+    
+    this.vmList = DNode.async(function (f) {
+        fs.readdir(vmDir, function (err,files) { f(files) });
+    });
+    
+    this.status = function (vm) {
+        return status[vm] || 'shutdown';
+    };
+    
+    this.start = function (vm) {
+        if (!(vm in status)) throw 'No such VM: ' + vm;
+        var qemu = proc.spawn('qemu', [ '-vnc', ':0.0', vmDir + '/' + vm ]);
+        qemu.addListener('exit', function () { status[vm] = 'shutdown' });
+        instances[vm] = qemu;
+        status[vm] = 'running';
+    };
+    
+    this.shutdown = function (vm) {
+        if (!(vm in status)) throw 'No such VM: ' + vm;
+        instances[vm].kill('SIGHUP');
+        status[vm] = 'shutdown';
+    };
+    
+    this.vmList(function (vms) {
+        vms.forEach(function (vm) {
+            status[vm] = 'stopped';
+        });
     });
 }
 
