cda136e9e1042fd9345b3c18d358c2a5186ca65d
James Halliday <substack@gmail.com>
2010-06-10 05:24:43 -0800

    split up manager into VM and Manager objects

--- bin/manager.js
+++ bin/manager.js
@@ -4,43 +4,76 @@ var fs = require('fs');
 var proc = require('child_process');
 var DNode = require('dnode').DNode;
 
+function VM (image) {
+    this.__defineGetter__('image', function () { return image });
+    
+    var port = null;
+    this.__defineGetter__('port', function () { return port });
+    
+    var power = 'off';
+    this.__defineGetter__('power', function () { return power });
+    
+    var pid = null;
+    
+    this.spawn = function (p) {
+        port = p;
+        power = 'on';
+        pid = proc.spawn(
+            'qemu', [ '-vnc', ':' + (p - 5900) + '.0', vmDir + '/' + vm ]
+        );
+        pid.addListener('exit', function () { power = 'off' });
+    };
+    
+    this.shutdown = function (vm) {
+        pid.kill('SIGHUP');
+    };
+}
+
+function elems (obj) {
+    var acc = [];
+    for (var i in obj) {
+        acc.push(obj[i]);
+    }
+    return acc;
+}
+
 function Manager () {
     var manager = this;
-    var status = {};
-    var instances = {};
+    var vms = {};
     var vmDir = __dirname + '/../vm';
     
+    function nextPort () {
+        var ports = elems(vms).map(function (vm) { return vm.port });
+        for (var port = 5900; port in ports; port++);
+        return port;
+    }
+    
     process.addListener('exit', function () {
-        for (var vm in instances) {
-            manager.stop(vm);
-        }
+        for (var i in vms) vms[i].stop();
     });
     
     this.vmList = DNode.async(function (f) {
         fs.readdir(vmDir, function (err,files) { f(files) });
     });
     
-    this.status = function (vm) {
-        return status[vm] || 'shutdown';
+    this.power = function (im) {
+        return vms[im].power;
     };
     
-    this.start = function (vm) {
-        if (!(vm in status)) throw 'No such VM: ' + vm;
-        var qemu = proc.spawn('qemu', [ '-vnc', ':0.0', vmDir + '/' + vm ]);
-        qemu.addListener('exit', function () { status[vm] = 'shutdown' });
-        instances[vm] = qemu;
-        status[vm] = 'running';
+    this.spawn = function (im) {
+        var port = nextPort();
+        vms[im].spawn(port);
+        return port;
+    };
+     
+    this.shutdown = function (im) {
+        vms[im].shutdown();
     };
     
-    this.shutdown = function (vm) {
-        if (!(vm in status)) throw 'No such VM: ' + vm;
-        instances[vm].kill('SIGHUP');
-        status[vm] = 'shutdown';
-    };
-    
-    this.vmList(function (vms) {
-        vms.forEach(function (vm) {
-            status[vm] = 'stopped';
+    // populate vms list
+    this.vmList(function (ims) {
+        ims.forEach(function (im) {
+            vms[im] = new VM(im);
         });
     });
 }
